288b8fda626810cf513c15fa31974c0eb1910e17
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-10 09:49:56 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    location.reload();
+}
+            
+function showDiv(elem){
+   if(elem.value == "ander")
+      document.getElementById("hidden_div").style.display = "block";
+}
