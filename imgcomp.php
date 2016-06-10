c723642cbccfaccd1d06c9c1690c87a5914dac7a
Jeroen Grooten <grootenjeroen@hotmail.com>
2016-06-10 09:24:52 +0200

    Added help and CSS

--- imgcomp.php
+++ imgcomp.php
@@ -24,12 +24,10 @@ echo '
     <head>
         <title>Comprimeer website foto\'s</title>
         <script>
-            
             function showDiv(elem){
                if(elem.value == "ander")
                   document.getElementById("hidden_div").style.display = "block";
             }
-            
         </script>
         <style>
             #letop{
@@ -39,27 +37,62 @@ echo '
                 -webkit-box-shadow: 1px 2px 3px rgba(0,0,0,.5);
                 box-shadow: 1px 2px 3px rgba(0,0,0,.5);
             }
+            
             #letop p{
                 padding-left: 10px;
                 padding-right: 10px;
             }
+
+            #help img{
+                float: left;
+                height: 20px;
+                width: 20px;
+                margin-top: 10px;
+            }
+            
+            #help_hidden{
+                max-width: 200px;
+                max-height: 200px;
+                margin-top: 10px;
+                margin-left: 50px;
+                display: none;
+                background-color: #DEDEDE;
+                border: 1.5px dashed black;
+                border-radius: 5px;
+            }
+            
+            #help_hidden p{
+                padding-left: 10px;
+                padding-right: 10px;
+            }
+            
+            #help:hover #help_hidden{
+                display: block;
+            }
+            
         </style>
+        <script>
+        function myFunction() {
+            location.reload();
+        }
+        </script>
     </head>
     <body>
         <h1><br>Websitefoto\'s comprimeren</h1>
         <div id="letop">
             <p><b>LET OP:</b> is de bestandnaam van de foto goed?</p>
-            <p>De bestandsnaam moet een foto omschrijving zijn of zoekterm in geval van SEO <br>Bijvoorbeeld een foto van een appeltaart moet de bestandsnaam <i>appeltaart.jpg</i> heben.</p>
+            <p>De bestandsnaam moet een foto omschrijving zijn of zoekterm in geval van SEO <br>Bijvoorbeeld een foto van een appeltaart moet de bestandsnaam <i>appaltaart.jpg</i> heben.</p>
         </div>
             
-        <form action="/wp-content/plugins/ImgComp/compress.php" enctype="multipart/form-data" method="post">
+        <form action="compress.php" enctype="multipart/form-data" method="post">
             <div>
                 <p>Selecteer uw foto\'s (maximaal 20 stuks):</p>
                 <input id="upload" name="upload[]" type="file" multiple="multiple" accept="image/*" />
                 <p>Selecteer het gewenste foto formaat:</p>
                 <select name="size" onchange="showDiv(this)">
-                    <option value="normaal">Normaal [800 x 600]</option>
-                    <option value="slider">Slider [1920 x 1080]</option>
+                    <option value="normaal">Normaal</option>
+                    <option value="slider">Slider</option>
+                    <option value="logo">Logo</option>
                     <option value="ander">Ander, Namelijk:</option>
                 </select>
                 <div id="hidden_div" style="display: none;">
@@ -69,10 +102,15 @@ echo '
                 </div>
             </div>
 
-            <p><input type="submit" name="submit" value="Verstuur"></p>
+            <p><input type="submit" name="submit" value="Verstuur" onclick="myFunction()"></p>
         </form>
+        <div id="help">
+            <img src="Images/question-mark.png" />
+            <div id="help_hidden"><p>Het kan enkele minuten duren om het bestand te uploaden, aanpassen en downloaden. Het kan zijn dat de pagina niet veranderd, maar de download start automatisch.</p></div>
+        </div>
     </body>
-</html>'
+</html>
+'
 ;
 
 }
